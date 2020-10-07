d2d0f006d1be35fbc9cc1d6614b169ac3e043c11
Atilio Villalba <atiliovg94@gmail.com>
2020-10-07 17:50:48 -0300

    added env ports and express initialization

---
+++ index.js
@@ -0,0 +1,18 @@
+//Libraries
+const express = require("express");
+require('dotenv').config();
+/*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*/
+// Initialize Express
+let app = express();
+// Configure middlewares
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+/*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*/
+// Configure routes
+
+/*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*/
+// Start the server
+app.listen(process.env.PORT || 3000, function() {
+  console.log("App listening on port " + process.env.PORT || 3000);
+});
